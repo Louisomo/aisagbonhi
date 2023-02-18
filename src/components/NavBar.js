c0d743e2a6eb2f0355a62ab54bd7c8fae0488c3f
louisomo <aisagbonhilouis@gmail.com>
2023-02-18 10:16:34 +0100

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -41,22 +41,24 @@ export const NavBar = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link as={Link} to="/#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link as={Link} to="/#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link as={Link} to="/" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link as={Link} to="/skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link as={Link} to="/projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
             </Nav>
             <span className="navbar-text">
-            <div className="social-icon">
-                <a href="https://github.com/louisomo"><FaGithub /></a>
-                <a href="https://twitter.com/louisfire07"><FaTwitter /></a>
-                <a href="https://www.instagram.com/louisfire7"><FaInstagram /></a>
-                <a href="https://www.linkedin.com/in/aisagbonhilouis"><FaLinkedin /></a>
-                <a href="https://wa.me/+2348143314075"><FaWhatsapp /></a>
-              </div>
-              <a href={aisagbonhi} target="_blank">
-  <button className="vvd"><span>Resume</span></button>
-</a> 
-            </span>
+  <div className="social-icon">
+    <a href="https://github.com/louisomo"><FaGithub /></a>
+    <a href="https://twitter.com/louisfire07"><FaTwitter /></a>
+    <a href="https://www.instagram.com/louisfire7"><FaInstagram /></a>
+    <a href="https://www.linkedin.com/in/aisagbonhilouis"><FaLinkedin /></a>
+    <a href="https://wa.me/+2348143314075"><FaWhatsapp /></a>
+  </div>
+</span>
+<div className="resume-btn-container">
+  <a href={aisagbonhi} target="_blank">
+    <button className="vvd"><span>Resume</span></button>
+  </a> 
+</div>
           </Navbar.Collapse>
         </Container>
       </Navbar>
